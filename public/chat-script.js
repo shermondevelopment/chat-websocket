323e171651709420ff1c7552f568a43e52bf10c2
Victor Shermon <victorshermon@yahoo.com>
2020-09-26 14:47:16 -0300

    chat-how-node

---
+++ public/chat-script.js
@@ -0,0 +1,10 @@
+class Chating {
+
+    constructor(campo, send, io) {
+      const $ = document.querySelector.bind(document);
+      this.campo = $(campo);
+      this.send = $(send);
+    }
+    
+
+}
---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const socketioJwt = require("socketio-jwt");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "..", "public")));
+
+app.set("views", path.resolve(__dirname, "..", "public"));
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+  response.render("index", { title: "victor" });
+});
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  socket.emit("previusMessages", messages);
+
+  socket.on("typingMessage", (data) => {
+    socket.broadcast.emit("message", data);
+  });
+
+  socket.on("mensagem", (data) => {
+    messages.push(data);
+    socket.broadcast.emit("mensagem", data);
+  });
+});
+
+server.listen(process.env.PORT || 3002, () => {
+  console.log("application start running");
+});
